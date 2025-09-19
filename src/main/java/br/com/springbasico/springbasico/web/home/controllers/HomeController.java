6d6896485808dea7c3a45153761703accb55403d
DBueno00 <165747110+DBueno00@users.noreply.github.com>
2025-09-19 01:32:38 -0300

    A
    Aprimotamento

--- src/main/java/br/com/springbasico/springbasico/web/home/controllers/HomeController.java
+++ src/main/java/br/com/springbasico/springbasico/web/home/controllers/HomeController.java
@@ -1,8 +1,14 @@
 package br.com.springbasico.springbasico.web.home.controllers;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Locale;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -13,5 +19,16 @@ public class HomeController {
     public String index(){
 
         return "home/index";
+    }
+
+    @GetMapping("/format-data")
+    public ModelAndView formatData(){
+        var model = Map.of("date", LocalDate.of(2024, 10, 10),
+        "locale", Locale.of("en-us"),
+        "datetime" , LocalDateTime.of(2024, 10, 10, 10, 10, 10),
+        "currency", 1024.51,
+        "percent", 2.0 /100);
+        return new ModelAndView("format-data",model);
+
     }
 }
